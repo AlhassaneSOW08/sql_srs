0285b78a5d0f77ce60d1a20052fb93fcc52720c7
Alhassane Sow <alhassanesow.ds@gmail.com>
2026-09-18 20:17:08 +0200

    end

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import logging
 import duckdb
 import streamlit as st
-
+from datetime import date, timedelta
 
 if "data" not in os.listdir():
     logging.error(os.listdir())
@@ -14,6 +14,31 @@ if "exercises_sql_tables.duckdb" not in os.listdir("data"):
     # subprocess.run(["python", "init_db.py"])
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
+
+def check_users_solution(user_query: str) -> None:
+    """
+    checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+
+    result = con.execute(user_query).df()
+    st.dataframe(result)
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(f"result has a {n_lines_difference} lines difference with solution_df")
 
 with st.sidebar:
     available_themes_df = con.execute("SELECT DISTINCT themes FROM memory_state").df()
@@ -29,7 +54,7 @@ with st.sidebar:
     else:
         select_exercise_query = "SELECT * FROM memory_state"
     exercise = (
-        con.execute(f"SELECT * FROM memory_state")
+        con.execute(select_exercise_query)
         .df()
         .sort_values("last_reviewed")
         .reset_index(drop=True)
@@ -43,20 +68,20 @@ with st.sidebar:
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
+
+
 if query:
-    result = con.execute(query).df()
-    st.dataframe(result)
+    check_users_solution(query)
 
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
+for n_days in [2, 7, 21]:
+    if st.button(f'revoir dans {n_days} jours'):
+        next_review = date.today() + timedelta(days=n_days)
+        con.execute(f"UPDATE memory_state SET last_reviewed = '{next_review}' WHERE exercise_name = '{exercise_name}'")
+        st.rerun()
 
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(f"result has a {n_lines_difference} lines difference with solution_df")
-
+if st.button('Reset'):
+    con.execute(f"UPDATE memory_state SET last_reviewed = '1970-01-01'")
+    st.rerun()
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
